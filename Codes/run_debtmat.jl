f65167b5669f06e15d56e045eec8c29037f52350
Francisco Roldan <fr673@nyu.edu>
2020-09-14 17:34:00 -0400

    fixed typos in sender

--- Codes/run_debtmat.jl
+++ Codes/run_debtmat.jl
@@ -3,14 +3,18 @@ include("debtmat.jl")
 function run_debtmat()
 
 	βvec = range(0.94, 0.99, length=11)
+	
+	dd = DebtMat()
 
 	for (jβ, βv) in enumerate(βvec)
 
-		dd = DebtMat(β = βv)
+		dd.pars[:β] = βv
 		write("../output.txt", "Created DebtMat with β = $βv")
+
 		equil!(dd)
 		write("../output.txt", "Solved DebtMat with β = $βv")
-		save("../Output/debtmat$(jj).jld", "dd", dd)
+
+		save("../Output/debtmat$(jβ).jld", "dd", dd)
 		write("../output.txt", "Saved DebtMat with β = $βv")
 	end
 end
